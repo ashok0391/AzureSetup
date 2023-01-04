91d2c8ac7d487d7a6eeebecbdc4a95b7f262a3da
ashok0391 <114929367+ashok0391@users.noreply.github.com>
2023-01-04 23:30:23 +0530

    Create params.sh

---
+++ params.sh
@@ -0,0 +1,22 @@
+## AKS
+LOCATION="westeurope"
+
+## Networking
+USERS_VNET="AK-VNET1"
+USERS_RG="AK-TESTRG"
+USERS_VNET_CIDR="10.100.0.0/16"
+USERS_SNET="AK-Subnet1"
+USERS_SNET_CIDR="10.100.100.0/24"
+
+
+
+## Jumpbox VM
+VM_NAME="vm-jumpbox"
+VM_IMAGE="UbuntuLTS"
+VM_SIZE="Standard_B1s"
+VM_OSD_SIZE="32"
+VM_RG=$USERS_RG
+VM_VNET=$USERS_VNET
+VM_SNET="jumpbox-subnet"
+VM_SNET_CIDR="10.100.110.0/28"
+VM_PUBIP="vm-jumpbox-pip"
