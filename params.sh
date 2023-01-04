8120c6105afdf2741900b26fe4b61051d2eacf6f
ashok0391 <114929367+ashok0391@users.noreply.github.com>
2023-01-05 00:12:57 +0530

    Update params.sh

--- params.sh
+++ params.sh
@@ -1,22 +1,3 @@
 ## AKS
 LOCATION="canadacentral"
 RG_NAME="TestRG"
-
-## Networking
-USERS_VNET="vnet-users"
-USERS_RG="users-rg"
-USERS_VNET_CIDR="10.100.0.0/16"
-USERS_SNET="users-subnet"
-USERS_SNET_CIDR="10.100.100.0/24"
-
-
-## Jumpbox VM
-VM_NAME="vm-jumpbox"
-VM_IMAGE="UbuntuLTS"
-VM_SIZE="Standard_B1s"
-VM_OSD_SIZE="32"
-VM_RG=$USERS_RG
-VM_VNET=$USERS_VNET
-VM_SNET="jumpbox-subnet"
-VM_SNET_CIDR="10.100.110.0/28"
-VM_PUBIP="vm-jumpbox-pip"
