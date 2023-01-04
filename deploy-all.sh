eb9027f9fb7b973584e524e72b606246c6c0bdfe
ashok0391 <114929367+ashok0391@users.noreply.github.com>
2023-01-05 00:11:33 +0530

    Update deploy-all.sh

--- deploy-all.sh
+++ deploy-all.sh
@@ -5,9 +5,3 @@ set -e
 echo "configuring Networking"
 ## create Resource Group for Users VNet
 az group create --name $USERS_RG --location $LOCATION
-
-## Create USERS VNet and SubNet
-az network vnet create \
-    -g $USERS_RG \
-    -n $USERS_VNET --address-prefix $USERS_VNET_CIDR \
-    --subnet-name $USERS_SNET --subnet-prefix $USERS_SNET_CIDR
