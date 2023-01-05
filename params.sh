9e9c97d97bc711cdaf443cff4e68135bc30219ee
ashok0391 <114929367+ashok0391@users.noreply.github.com>
2023-01-05 07:19:45 +0530

    Update params.sh
    updated

--- params.sh
+++ params.sh
@@ -1,3 +1,10 @@
 ## AKS
-LOCATION="canadacentral"
-RG_NAME="TestRG"
+LOCATION="eastus2"
+RG_NAME="TestingRG"
+
+## Networking
+USERS_VNET="TestingVNet"
+USERS_RG="testingnetwork-rg"
+USERS_VNET_CIDR="10.100.0.0/16"
+USERS_SNET="users-subnet"
+USERS_SNET_CIDR="10.100.100.0/24"
